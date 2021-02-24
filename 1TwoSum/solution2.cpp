4cf1e8fe0fbae324c6800b3c24b4bb0edd2e9bb2
Anthony860810 <kuotony860810@gmail.com>
2021-02-24 20:27:27 +0800

    TwoSum sol2

---
+++ 1TwoSum/solution2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> Map;
+        vector<int> ans;
+        for(int idx=0; idx<nums.size(); idx++){
+            int complement = target-nums[idx];
+            //cout<<nums[idx]<<" ";
+            if (Map.count(complement)==0){
+                Map.insert(pair<int,int>(nums[idx],idx));
+            }
+            else{
+                ans.push_back(Map[complement]);
+                ans.push_back(idx);
+            }
+        }
+        return ans;
+    }
+};
