ff147ce6ad19030a66cb5b984a8e81b58eb529af
Anthony860810 <kuotony860810@gmail.com>
2021-04-03 10:52:35 +0800

    9. Palindrome Number

--- 9Palindrome Number/solution1.c
+++ 9Palindrome Number/solution1.c
@@ -2,11 +2,11 @@ bool isPalindrome(int x){
     if(x<0)
         return false;
     unsigned long reverse=0;
-    int tmp=x;
-    while(tmp){
+    int temp=x;
+    while(temp){
        reverse*=10;
-       reverse+=(tmp%10);
-        tmp/=10;
+       reverse+=(temp%10);
+        temp/=10;
     }
     return reverse==x;
 }
